eeb5ba38112fd5ba06c26a2f158092eceed56e82
DownChapel <DownChapel@gmail.com>
2024-11-21 14:34:14 +0000

    Style set user page

--- src/routes/set-customer/+page.svelte
+++ src/routes/set-customer/+page.svelte
@@ -4,28 +4,69 @@
 	let { data } = $props();
 </script>
 
-{#if data.customer}
-	<p>Current Customer: {data.customer.email}</p>
-	<form method="post" action="?/clearCustomer" use:enhance>
-		<button type="submit">Clear Customer</button>
+<div class="mx-auto max-w-2xl px-4 pt-10">
+	<div class="text-center">
+		<h1 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Set Customer</h1>
+
+		<p class="mt-2 text-sm text-gray-500">
+			Use this page to set a customer on the cart. This simulates logging in as the user
+		</p>
+
+		<p class="mt-2 text-sm text-gray-500">
+			If you don't have a cart, the user will be set on the cart when the cart is created
+		</p>
+	</div>
+
+	{#if data.customer}
+		<div class="flex items-center justify-center bg-white p-8">
+			<div class="mx-auto flex w-full max-w-xs flex-col gap-4 rounded-md border p-4">
+				<h2 class="text-base/7 font-semibold text-gray-900">Current Customer</h2>
+				<p class="mt-1 text-sm/6 text-gray-600">{data.customer.email}</p>
+				<form method="post" action="?/clearCustomer" use:enhance>
+					<button
+						type="submit"
+						class="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm/6 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+						>Clear Current Customer</button
+					>
+				</form>
+			</div>
+		</div>
+	{/if}
+
+	<form
+		method="post"
+		action="?/setCustomer"
+		use:enhance={() => {
+			return async ({ update, result }) => {
+				// if (result.type === 'success' && result.data?.cart) {
+				// 	console.log(result.data.cart.);
+				// }
+
+				await update();
+			};
+		}}
+	>
+		<div class="flex items-center justify-center bg-white p-8">
+			<div class="mx-auto flex w-full max-w-xs flex-col gap-4 rounded-md border p-4">
+				<h2 class="text-base/7 font-semibold text-gray-900">Set Customer</h2>
+				<div>
+					<label for="email" class="block text-sm/6 font-medium text-gray-900">Email</label>
+					<input
+						type="text"
+						id="email"
+						name="email"
+						class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm/6"
+						required
+					/>
+				</div>
+				<div>
+					<button
+						type="submit"
+						class="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm/6 font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+						>Set Customer</button
+					>
+				</div>
+			</div>
+		</div>
 	</form>
-{/if}
-
-<form
-	method="post"
-	action="?/setCustomer"
-	use:enhance={() => {
-		return async ({ update, result }) => {
-			// if (result.type === 'success' && result.data?.cart) {
-			// 	console.log(result.data.cart.);
-			// }
-
-			await update();
-		};
-	}}
->
-	<label for="email">Customer Email:</label>
-	<input type="text" id="email" name="email" value="stephen.upchurch@dovetech.com" required />
-
-	<button type="submit">Set Customer</button>
-</form>
+</div>
