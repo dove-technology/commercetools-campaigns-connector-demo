3defe41b8ba717d3518bf9fdaec8bbdc652539cc
DownChapel <DownChapel@gmail.com>
2024-11-22 15:40:01 +0000

    Line item total tests

--- src/lib/CartHelpers.test.ts
+++ src/lib/CartHelpers.test.ts
@@ -1,8 +1,8 @@
 import { it, expect } from 'vitest';
-import { getLineItemSubtotal } from './CartHelpers';
+import { getLineItemSubtotal, getLineItemTotal } from './CartHelpers';
 import type { Cart } from '@commercetools/platform-sdk';
 import * as cartWithProductDiscount from './test-carts/cart-with-product-discount.json';
-
+import * as cartWithLineItemDiscounts from './test-carts/cart-with-line-item-discounts.json';
 import * as cartWithNoDiscounts from './test-carts/cart-with-no-discounts.json';
 
 it('line item subtotal should take into account quantity', () => {
@@ -21,4 +21,22 @@ it('line item subtotal should take into account product discounts', () => {
 	const subtotal = getLineItemSubtotal(lineItem);
 
 	expect(subtotal).toBe(11049);
+});
+
+it('line item total should return total', () => {
+	const cart = cartWithNoDiscounts as Cart;
+
+	const lineItem = cart.lineItems[0];
+	const total = getLineItemTotal(lineItem);
+
+	expect(total).toBe(598);
+});
+
+it('line item total should take into account price per quantity', () => {
+	const cart = cartWithLineItemDiscounts as Cart;
+
+	const lineItem = cart.lineItems[0];
+	const total = getLineItemTotal(lineItem);
+
+	expect(total).toBe(598);
 });
