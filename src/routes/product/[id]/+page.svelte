49e974aa9247f019dfc85be7a04d706e2e0caa41
DownChapel <DownChapel@gmail.com>
2024-11-25 10:19:08 +0000

    Support multiple variants

--- src/routes/product/[id]/+page.svelte
+++ src/routes/product/[id]/+page.svelte
@@ -7,8 +7,9 @@
 
 	let { data, form } = $props();
 
-	let selectedVariantSku = data.product.masterVariant.sku;
+	const allVariants = [data.product.masterVariant, ...data.product.variants];
 	let addingToCart = $state(false);
+	let selectedVariant = $state(data.product.masterVariant);
 </script>
 
 <main class="mx-auto mt-8 max-w-2xl px-4 pb-16 sm:px-6 sm:pb-24 lg:max-w-7xl lg:px-8">
@@ -17,8 +18,8 @@
 			<div class="flex justify-between">
 				<h1 class="text-xl font-medium text-gray-900">{data.product.name[data.currentLanguage]}</h1>
 				<p class="text-xl font-medium text-gray-900">
-					{#if data.product.masterVariant.price}
-						<ProductPrice price={data.product.masterVariant.price} />
+					{#if selectedVariant.price}
+						<ProductPrice price={selectedVariant.price} />
 					{/if}
 				</p>
 			</div>
@@ -53,16 +54,52 @@
 					};
 				}}
 			>
-				<input type="hidden" name="sku" value={selectedVariantSku} />
+				{#if allVariants.length > 1}
+					<div class="mt-4">
+						<label for="sku" class="block text-sm font-medium text-gray-700">Variant</label>
+						<select
+							id="sku"
+							name="sku"
+							class="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+							onchange={(event) => {
+								const selectedSku = (event.target as HTMLSelectElement).value;
+
+								const newSelectedVariant = allVariants.find(
+									(variant) => variant.sku === selectedSku
+								);
+
+								if (newSelectedVariant) {
+									selectedVariant = newSelectedVariant;
+								} else {
+									throw new Error('Selected variant not found');
+								}
+							}}
+						>
+							{#each allVariants as variant}
+								<option value={variant.sku}>{variant.sku}</option>
+							{/each}
+						</select>
+					</div>
+				{:else}
+					<input type="hidden" name="sku" value={selectedVariant.sku} />
+				{/if}
+
 				<button
 					type="submit"
-					disabled={addingToCart}
+					disabled={addingToCart || !selectedVariant.price}
 					class="mt-8 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50"
 					>Add to cart</button
 				>
 				{#if form?.addToCartError}
 					<p class="mt-2 text-sm text-red-600">
 						{form.addToCartError}
+					</p>
+				{/if}
+
+				{#if !selectedVariant.price}
+					<p class="mt-2 text-sm text-red-600">
+						No matching price for the current context. Check the product price configuration (e.g.
+						is there a valid entry for your currency and country selection?).
 					</p>
 				{/if}
 			</form>
